f6ff6279c4055e3fac9fd0f075f97d46406da614
AlwinRicardoFiebes <alwinricardofiebes007@gmail.com>
2022-02-10 17:59:41 -0300

    Finalização da lista de tarefas react

--- src/components/Todoform.js
+++ src/components/Todoform.js
@@ -1,16 +1,32 @@
 import React, {useState} from "react";
 
-function TodoForm() {
-const [input, setInput] = useState('')
+function TodoForm(props) {
+const [input, setInput] = useState('');
+
+const handlechange= e => {
+    setInput(e.target.value)
+}
+
+const handleSubmit = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+        id: Math.floor(Math.random() * 100000),
+        text: input
+    });
+
+   setInput('') 
+};
 
     return (
-        <form className='todo-from'>
+        <form className='todo-from' onSubmit={handleSubmit}>
             <input 
             type='text' 
             placeholder='Adicione uma Tarefa'
             value={input}
             name='text'
-            classname='todo-input'
+            className='todo-input'
+            onChange={handlechange}
             />
             <button className="todo-button">Adicionar Tarefa</button>
         </form>
